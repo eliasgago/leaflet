d66d36f73ec48e99a2c1436e708bea28c101a1a6
Elías Gago <elias.gago@gmail.com>
2017-06-22 17:37:39 +0200

    Wineries

--- js/map-load.js
+++ js/map-load.js
@@ -152,31 +152,71 @@ for (var i = sites.length - 1; i >= 0; i--) {
 
 var lodgingsIcon = L.icon({
     iconUrl: './img/lodgings_icon.png',
-    iconSize: [40, 40],
+    iconSize: [25, 25],
     iconAnchor: [20, 20],
     popupAnchor: [-3, -76]
 });
 
 var lodgings = [
     {
+        "id": "donatus",
         "name" : "Vila Donatus",
-        "lat": 42.41451442176539,
-        "lon": -7.4793291091918945
+        "lat": 42.41389658591989,
+        "lon": -7.478824853897094
     },
     {
+        "id": "corona",
         "name" : "Casa Corona",
         "lat": 42.45853538959867,
         "lon": -7.597963213920593
     },
     {
+        "id": "flores",
         "name" : "Casa das Flores",
         "lat": 42.44503822748129,
         "lon": -7.573903799057006
     },
     {
+        "id": "estevo",
         "name" : "Casa do Estevo",
         "lat": 42.42134783469654,
         "lon": -7.472907900810241
+    },
+    {
+        "id": "rio_sil",
+        "name" : "Apartamentos Río Sil",
+        "lat": 42.4166946207761,
+        "lon": -7.597289979457854
+    },
+    {
+        "id": "madrina",
+        "name" : "Casa da Madriña",
+        "lat": 42.48857888524799,
+        "lon": -7.574032545089722
+    },
+    {
+        "id": "anllo",
+        "name" : "Rectoral de Anllo",
+        "lat": 42.43931391926548,
+        "lon": -7.626260519027709
+    },
+    {
+        "id": "rosende",
+        "name" : "Casa Grande de Rosende",
+        "lat": 42.45652089523697,
+        "lon": -7.620472311973571
+    },
+    {
+        "id": "ruperto",
+        "name" : "Casa Ruperto",
+        "lat": 42.414728286601516,
+        "lon": -7.479715347290039
+    },
+    {
+        "id": "solveira",
+        "name" : "Casa Solveira",
+        "lat": 42.417710436936574,
+        "lon": -7.600843906402588
     }
 ]
 
@@ -192,7 +232,129 @@ for (var i = lodgings.length - 1; i >= 0; i--) {
     markerPoints.push(marker);
 }   
 
+
+var wineriesIcon = L.icon({
+    iconUrl: './img/wineries_icon.png',
+    iconSize: [25, 25],
+    iconAnchor: [20, 20],
+    popupAnchor: [-3, -76]
+});
+
+var wineries = [
+    {
+        "id": "algueira",
+        "name" : "Algueira",
+        "lat": 42.41797181387165,
+        "lon": -7.474597692489625
+    },
+    {
+        "id": "lobeiras",
+        "name" : "Lobeiras",
+        "lat": 42.42024099511087,
+        "lon": -7.579439878463744
+    },
+    {
+        "id": "malcavada",
+        "name" : "Malcavada",
+        "lat": 42.45669899656409,
+        "lon": -7.619297504425049
+    },
+    {
+        "id": "marcelino",
+        "name" : "Marcelino I",
+        "lat": 42.439955257818234,
+        "lon": -7.582336664199829
+    },
+    {
+        "id": "proencia",
+        "name" : "Proencia",
+        "lat": 42.45328332060084,
+        "lon": -7.588167786598205
+    },
+    {
+        "id": "soutelo",
+        "name" : "Soutelo",
+        "lat": 42.42317932418995,
+        "lon": -7.540912628173828
+    },
+    {
+        "id": "petron",
+        "name" : "Petrón",
+        "lat": 42.41838367834631,
+        "lon": -7.474393844604492
+    },
+    {
+        "id": "rectoral",
+        "name" : "Rectoral de Amandi",
+        "lat": 42.39905877681099,
+        "lon": -7.515850067138671
+    },
+    {
+        "id": "cazoga",
+        "name" : "Viña Cazoga",
+        "lat": 42.39912215986002,
+        "lon": -7.507599592208861
+    },
+    {
+        "id": "noguedo",
+        "name" : "Casa Noguedo",
+        "lat": 42.407476277594995,
+        "lon": -7.559446692466736
+    },
+    {
+        "id": "sesse",
+        "name" : "Castro Sesse",
+        "lat": 42.483000978057134,
+        "lon": -7.589557170867919
+    },
+    {
+        "id": "sesse",
+        "name" : "Castro Sesse",
+        "lat": 42.483000978057134,
+        "lon": -7.589557170867919
+    },
+    {
+        "id": "lucenza",
+        "name" : "Lucenza",
+        "lat": 42.441823811732505,
+        "lon": -7.588908076286315
+    },
+    {
+        "id": "estrela",
+        "name" : "Estrela",
+        "lat": 42.41708075166462,
+        "lon": -7.519959211349487
+    },
+    {
+        "id": "bernardino",
+        "name" : "Don Bernardino",
+        "lat": 42.42111221216409,
+        "lon": -7.528767585754394
+    },
+    {
+        "id": "san_xiao",
+        "name" : "San Xiao",
+        "lat": 42.40705245867436,
+        "lon": -7.530033588409423
+    }
+];
+
+var wineriesGroup = [];
+
+for (var i = wineries.length - 1; i >= 0; i--) {
+    var options = {
+        markerId: 'wineries_' + wineries[i].id,
+        type: 'wineries',
+        title: wineries[i].name
+    };
+    var marker = L.marker([wineries[i].lat, wineries[i].lon], options).addTo(map);
+    marker.setIcon(wineriesIcon);
+    marker.on('click', show);
+    wineriesGroup.push(marker);
+}   
+
 var points = L.layerGroup(markerPoints).addTo(map); 
+var wineriesPoints = L.layerGroup(wineriesGroup).addTo(map); 
 
 map.on('zoomend', function() {
     console.log(map.getZoom());
