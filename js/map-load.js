fa3593b21c77c42110f2ca243c1aeb39a4bc614d
Elías Gago <egago@fidesol.org>
2017-06-28 13:35:14 +0200

    Lodgings list

--- js/map-load.js
+++ js/map-load.js
@@ -27,49 +27,49 @@ var myIcon2 = L.icon({
 
 var viewpoints = [
     {
-        "id": "os_chancis",
+        "id": "viewpoints_os_chancis",
         "name" : "Miradoiro da Os Chancís",
         "lat": 42.411231108946076,
         "lon": -7.634741663932801
     },
     {
-        "id": "cividade",
+        "id": "viewpoints_cividade",
         "name" : "Miradoiro da Cividade",
         "lat": 42.39334215481423,
         "lon": -7.615011334419251
     },
     {
-        "id": "boqueirino",
+        "id": "viewpoints_boqueirino",
         "name" : "Miradoiro do Boqueiriño",
         "lat": 42.40393908425197,
         "lon": -7.596171498298644
     },
     {
-        "id": "santiorxo",
+        "id": "viewpoints_santiorxo",
         "name" : "Miradoiro de Santiorxo",
         "lat": 42.40786840656946,
         "lon": -7.568507194519042
     },
     {
-        "id": "cadeiras",
+        "id": "viewpoints_cadeiras",
         "name" : "Miradoiro de Cadeiras",
         "lat": 42.3976841411843,
         "lon": -7.555275857448577
     },
     {
-        "id": "os_chelos",
+        "id": "viewpoints_os_chelos",
         "name" : "Miradoiro de Os Chelos",
         "lat": 42.38953276520176,
         "lon": -7.517826855182647
     },
     {
-        "id": "souto_chao",
+        "id": "viewpoints_souto_chao",
         "name" : "Miradoiro de Souto Chao",
         "lat": 42.407500043057176,
         "lon": -7.471837699413299
     },
     {
-        "id": "pena_do_castelo",
+        "id": "viewpoints_pena_do_castelo",
         "name" : "Miradoiro da Pena do Castelo",
         "lat": 42.412528216274005,
         "lon": -7.467776834964752
@@ -81,9 +81,14 @@ function show(e) {
     var markerId = marker.options.markerId;
     var type = marker.options.type;
     sidebar.open(type);
-    $('.level1').hide();
-    $('.level2').hide();
-    $('#' + markerId).show(1000);
+    var newActivePane = $('#' + markerId);
+    var oldActivePane = newActivePane.parent().find('.sidebar-pane-detail.active');
+    oldActivePane.hide('slide', 600);
+    setTimeout(function() { 
+        oldActivePane.removeClass('active');
+        newActivePane.show('slide', 600); 
+        newActivePane.addClass('active');
+    }, 620);
     $('.icon-active').removeClass('icon-active');
     $(marker._icon).addClass('icon-active');
 }
@@ -99,7 +104,7 @@ var viewpointsMarkers = [];
 
 for (var i = viewpoints.length - 1; i >= 0; i--) {
     var options = {
-        markerId: 'viewpoints_' + viewpoints[i].id,
+        markerId: viewpoints[i].id,
         type: 'viewpoints',
         title: viewpoints[i].name
     };
@@ -112,49 +117,49 @@ for (var i = viewpoints.length - 1; i >= 0; i--) {
 
 var sites = [
     {
-        "id": "oficina",
+        "id": "sites_oficina",
         "name" : "Oficina de Turismo",
         "icon": "./img/info_icon.png",
         "lat": 42.46204971576779,
         "lon": -7.587255835533141
     },
     {
-        "id": "siltrip",
+        "id": "sites_siltrip",
         "name" : "SilTrip",
         "icon": "./img/siltrip_icon.png",
         "lat": 42.409381446908135,
         "lon": -7.632086277008057
     },
     {
-        "id": "catamaran",
+        "id": "sites_catamaran",
         "name" : "Catamarán",
         "icon": "./img/catamaran_icon.png",
         "lat": 42.4095794861362,
         "lon": -7.444020509719849
     },
     {
-        "id": "xabrega",
+        "id": "sites_xabrega",
         "name" : "Os Muiños do Xabrega",
         "icon": "./img/xabrega_icon.png",
         "lat": 42.41889454494551,
         "lon": -7.630434036254883
     },
     {
-        "id": "proendos",
+        "id": "sites_proendos",
         "name" : "Petroglifos de Proendos",
         "icon": "./img/proendos_icon.png",
         "lat": 42.452262143758645,
         "lon": -7.586199045181274
     },
     {
-        "id": "gundivos",
+        "id": "sites_gundivos",
         "name" : "Centro Oleiro Rectoral de Gundivós",
         "icon": "./img/gundivos_icon.png",
         "lat": 42.446261403946686,
         "lon": -7.5407034158706665
     },
     {
-        "id": "lobios",
+        "id": "sites_lobios",
         "name" : "Igrexa de San Xillao de Lobios",
         "icon": "./img/lobios_icon.png",
         "lat": 42.40813774626316,
@@ -173,7 +178,7 @@ var sitesMarkers = [];
 
 for (var i = sites.length - 1; i >= 0; i--) {
     var options = {
-        markerId: 'sites_' + sites[i].id,
+        markerId: sites[i].id,
         type: 'sites',
         title: sites[i].name
     };
@@ -196,61 +201,61 @@ for (var i = sites.length - 1; i >= 0; i--) {
 
 var lodgings = [
     {
-        "id": "donatus",
+        "id": "lodgings_donatus",
         "name" : "Vila Donatus",
         "lat": 42.41389658591989,
         "lon": -7.478824853897094
     },
     {
-        "id": "corona",
+        "id": "lodgings_corona",
         "name" : "Casa Corona",
         "lat": 42.45853538959867,
         "lon": -7.597963213920593
     },
     {
-        "id": "flores",
+        "id": "lodgings_flores",
         "name" : "Casa das Flores",
         "lat": 42.44503822748129,
         "lon": -7.573903799057006
     },
     {
-        "id": "estevo",
+        "id": "lodgings_estevo",
         "name" : "Casa do Estevo",
         "lat": 42.42134783469654,
         "lon": -7.472907900810241
     },
     {
-        "id": "rio_sil",
+        "id": "lodgings_rio_sil",
         "name" : "Apartamentos Río Sil",
         "lat": 42.4166946207761,
         "lon": -7.597289979457854
     },
     {
-        "id": "madrina",
+        "id": "lodgings_madrina",
         "name" : "Casa da Madriña",
         "lat": 42.48857888524799,
         "lon": -7.574032545089722
     },
     {
-        "id": "anllo",
+        "id": "lodgings_anllo",
         "name" : "Rectoral de Anllo",
         "lat": 42.43931391926548,
         "lon": -7.626260519027709
     },
     {
-        "id": "rosende",
+        "id": "lodgings_rosende",
         "name" : "Casa Grande de Rosende",
         "lat": 42.45652089523697,
         "lon": -7.620472311973571
     },
     {
-        "id": "ruperto",
+        "id": "lodgings_ruperto",
         "name" : "Casa Ruperto",
         "lat": 42.414728286601516,
         "lon": -7.479715347290039
     },
     {
-        "id": "solveira",
+        "id": "lodgings_solveira",
         "name" : "Casa Solveira",
         "lat": 42.417710436936574,
         "lon": -7.600843906402588
@@ -268,7 +273,7 @@ var lodgingsMarkers = [];
 
 for (var i = lodgings.length - 1; i >= 0; i--) {
     var options = {
-        markerId: 'lodgings_' + lodgings[i].id,
+        markerId: lodgings[i].id,
         type: 'lodgings',
         title: lodgings[i].name
     };
@@ -282,205 +287,199 @@ for (var i = lodgings.length - 1; i >= 0; i--) {
 
 var wineries = [
     {
-        "id": "algueira",
+        "id": "wineries_algueira",
         "name" : "Algueira",
         "lat": 42.41797181387165,
         "lon": -7.474597692489625
     },
     {
-        "id": "lobeiras",
+        "id": "wineries_lobeiras",
         "name" : "Lobeiras",
         "lat": 42.42024099511087,
         "lon": -7.579439878463744
     },
     {
-        "id": "malcavada",
+        "id": "wineries_malcavada",
         "name" : "Malcavada",
         "lat": 42.45669899656409,
         "lon": -7.619297504425049
     },
     {
-        "id": "marcelino",
+        "id": "wineries_marcelino",
         "name" : "Marcelino I",
         "lat": 42.439955257818234,
         "lon": -7.582336664199829
     },
     {
-        "id": "proencia",
+        "id": "wineries_proencia",
         "name" : "Proencia",
         "lat": 42.45328332060084,
         "lon": -7.588167786598205
     },
     {
-        "id": "soutelo",
+        "id": "wineries_soutelo",
         "name" : "Soutelo",
         "lat": 42.42317932418995,
         "lon": -7.540912628173828
     },
     {
-        "id": "petron",
+        "id": "wineries_petron",
         "name" : "Petrón",
         "lat": 42.41838367834631,
         "lon": -7.474393844604492
     },
     {
-        "id": "rectoral",
+        "id": "wineries_rectoral",
         "name" : "Rectoral de Amandi",
         "lat": 42.39905877681099,
         "lon": -7.515850067138671
     },
     {
-        "id": "cazoga",
+        "id": "wineries_cazoga",
         "name" : "Viña Cazoga",
         "lat": 42.39912215986002,
         "lon": -7.507599592208861
     },
     {
-        "id": "noguedo",
+        "id": "wineries_noguedo",
         "name" : "Casa Noguedo",
         "lat": 42.407476277594995,
         "lon": -7.559446692466736
     },
     {
-        "id": "sesse",
+        "id": "wineries_sesse",
         "name" : "Castro Sesse",
         "lat": 42.483000978057134,
         "lon": -7.589557170867919
     },
     {
-        "id": "sesse",
-        "name" : "Castro Sesse",
-        "lat": 42.483000978057134,
-        "lon": -7.589557170867919
-    },
-    {
-        "id": "lucenza",
+        "id": "lwineries_ucenza",
         "name" : "Lucenza",
         "lat": 42.441823811732505,
         "lon": -7.588908076286315
     },
     {
-        "id": "estrela",
+        "id": "wineries_estrela",
         "name" : "Estrela",
         "lat": 42.41708075166462,
         "lon": -7.519959211349487
     },
     {
-        "id": "bernardino",
+        "id": "wineries_bernardino",
         "name" : "Don Bernardino",
         "lat": 42.42111221216409,
         "lon": -7.528767585754394
     },
     {
-        "id": "san_xiao",
+        "id": "wineries_san_xiao",
         "name" : "San Xiao",
         "lat": 42.40705245867436,
         "lon": -7.530033588409423
     },
     {
-        "id": "frieira",
+        "id": "wineries_frieira",
         "name" : "Viña A Frieira",
         "lat": 42.418565848308745,
         "lon": -7.5054484605789185
     },
     {
-        "id": "cichon",
+        "id": "wineries_cichon",
         "name" : "Viña Cichón",
         "lat": 42.41423322800066,
         "lon": -7.48068630695343
     },
     {
-        "id": "cruceiro",
+        "id": "wineries_cruceiro",
         "name" : "Cruceiro",
         "lat": 42.41749658227026,
         "lon": -7.504348754882812
     },
     {
-        "id": "anzio",
+        "id": "wineries_anzio",
         "name" : "Anzio",
         "lat": 42.41423322800066,
         "lon": -7.48068630695343
     },
     {
-        "id": "barbado",
+        "id": "wineries_barbado",
         "name" : "Barbado",
         "lat": 42.453378312949006,
         "lon": -7.585823535919189
     },
     {
-        "id": "gullufre",
+        "id": "wineries_gullufre",
         "name" : "Gullufre",
         "lat": 42.40739309840652,
         "lon": -7.533617019653319
     },
     {
-        "id": "regoa",
+        "id": "wineries_regoa",
         "name" : "Régoa",
         "lat": 42.3964600166767,
         "lon": -7.550182342529296
     },
     {
-        "id": "rectoral_gundivos",
+        "id": "wineries_rectoral_gundivos",
         "name" : "Rectoral de Gundivós",
         "lat": 42.41701738675883,
         "lon": -7.5199806690216064
     },
     {
-        "id": "regina",
+        "id": "wineries_regina",
         "name" : "Regina Viarum",
         "lat": 42.40862295088143,
         "lon": -7.476593255996704
     },
     {
-        "id": "decima",
+        "id": "wineries_decima",
         "name" : "Décima",
         "lat": 42.419306403359556,
         "lon": -7.501140832901
     },
     {
-        "id": "val_de_lenda",
+        "id": "wineries_val_de_lenda",
         "name" : "Val de Lenda",
         "lat": 42.40719505227568,
         "lon": -7.513527274131775
     },
     {
-        "id": "peon",
+        "id": "wineries_peon",
         "name" : "Viña Peón",
         "lat": 42.42992666380791,
         "lon": -7.570309638977051
     },
     {
-        "id": "regueiral",
+        "id": "wineries_regueiral",
         "name" : "Viña Regueiral",
         "lat": 42.420767686867336,
         "lon": -7.58104920387268
     },
     {
-        "id": "guimaro",
+        "id": "wineries_guimaro",
         "name" : "Guímaro",
         "lat": 42.42975244586277,
         "lon": -7.51813530921936
     },
     {
-        "id": "tear",
+        "id": "wineries_tear",
         "name" : "Tear",
         "lat": 42.43259926477228,
         "lon": -7.572535872459411
     },
     {
-        "id": "baldomero",
+        "id": "wineries_baldomero",
         "name" : "Baldomero",
         "lat": 42.409634937008036,
         "lon": -7.520511746406556
     },
     {
-        "id": "cividade",
+        "id": "wineries_cividade",
         "name" : "Cividade",
         "lat": 42.41473620750738,
         "lon": -7.590157985687256
     },
     {
-        "id": "mezquita",
+        "id": "wineries_mezquita",
         "name" : "Mezquita",
         "lat": 42.4125777236558,
         "lon": -7.509214282035828
@@ -498,7 +497,7 @@ var wineriesMarkers = [];
 
 for (var i = wineries.length - 1; i >= 0; i--) {
     var options = {
-        markerId: 'wineries_' + wineries[i].id,
+        markerId: wineries[i].id,
         type: 'wineries',
         title: wineries[i].name
     };
@@ -515,42 +514,22 @@ var lodgingsLayer = L.layerGroup(lodgingsMarkers).addTo(map);
 var wineriesLayer = L.layerGroup(wineriesMarkers).addTo(map); 
 map.removeLayer(wineriesLayer);
 
-map.on('zoomend', function() {
-    console.log(map.getZoom());
-    if (map.getZoom() <12){
-        if (map.hasLayer(viewpointsLayer)) {
-            map.removeLayer(viewpointsLayer);
-        }
-        if (map.hasLayer(sitesLayer)) {
-            map.removeLayer(sitesLayer);
-        }
-        if (map.hasLayer(lodgingsLayer)) {
-            map.removeLayer(lodgingsLayer);
-        }
-        if (map.hasLayer(wineriesLayer)) {
-            map.removeLayer(wineriesLayer);
-        }
-    }
-    if (map.getZoom() >= 12){
-        if (!map.hasLayer(viewpointsLayer)){
-            map.addLayer(viewpointsLayer);
-        }
-        if (!map.hasLayer(sitesLayer)){
-            map.addLayer(sitesLayer);
-        }
-        if (!map.hasLayer(lodgingsLayer)){
-            map.addLayer(lodgingsLayer);
-        }
-        if (!map.hasLayer(wineriesLayer)){
-            map.addLayer(wineriesLayer);
-        }
-    }
-});
-
+var markers = viewpointsMarkers.concat(sitesMarkers).concat(lodgingsMarkers).concat(wineriesMarkers);
 
 // marker.bindPopup("<b>Miradoiro da Cividade!</b><br>Vaia vistas!");
 
-function find(id){
+function findMarker(id) {
+    id = id.substring(1, id.length);
+    console.log(id);
+    for (var i = markers.length - 1; i >= 0; i--) {
+        console.log(markers[i].options.markerId);
+        if(markers[i].options.markerId == id){
+            return markers[i];
+        }
+    }
+}
+
+function find(id) {
     for (var i = viewpoints.length - 1; i >= 0; i--) {
         if(viewpoints[i].id == id){
             return viewpoints[i];
@@ -589,39 +568,96 @@ function showRoute(destinationId) {
     route.addTo(map);
 }
 
-$('.level2').hide();
-
 $(document).on("click", ".link", function (e) {
     //marker.setIcon(myIcon2);
-    var id = '#viewpoints_' + e.currentTarget.id;
-    $('.level1').hide(1000);
-    $('.level2').hide(1000);
-    $(id).show(1000);
+    var id = e.currentTarget.getAttribute("href");
+    var newActivePane = $(id);
+    var oldActivePane = $(id).parent().find('.sidebar-pane-detail.active');
+    oldActivePane.hide('slide', 400);
+    setTimeout(function() { 
+        oldActivePane.removeClass('active');
+        newActivePane.show('slide', 600); 
+        newActivePane.addClass('active');
+    }, 420);
+    $('.icon-active').removeClass('icon-active');
+    var marker = findMarker(id);
+    $(marker._icon).addClass('icon-active');
     //marker._icon.classList.add("className");
 });
 
-$(document).on("click", ".button_back", function () {
-    //marker.setIcon(myIcon2);
-    $('.level2').hide(1000);
-    $('#viewpoints_list').show(1000);
+var MAX_VISIBLE_ITEMS_ZOOM_LEVEL = 12;
+
+map.on('zoomend', function() {
+    if (map.getZoom() < MAX_VISIBLE_ITEMS_ZOOM_LEVEL){
+        if (map.hasLayer(viewpointsLayer)) {
+            map.removeLayer(viewpointsLayer);
+        }
+        if (map.hasLayer(sitesLayer)) {
+            map.removeLayer(sitesLayer);
+        }
+        if (map.hasLayer(lodgingsLayer)) {
+            map.removeLayer(lodgingsLayer);
+        }
+        if (map.hasLayer(wineriesLayer)) {
+            map.removeLayer(wineriesLayer);
+        }
+    }
+    if (map.getZoom() >= MAX_VISIBLE_ITEMS_ZOOM_LEVEL){
+        if (!map.hasLayer(viewpointsLayer) && isLayerVisible('viewpoints')){
+            map.addLayer(viewpointsLayer);
+        }
+        if (!map.hasLayer(sitesLayer) && isLayerVisible('sites')){
+            map.addLayer(sitesLayer);
+        }
+        if (!map.hasLayer(lodgingsLayer) && isLayerVisible('lodgings')){
+            map.addLayer(lodgingsLayer);
+        }
+        if (!map.hasLayer(wineriesLayer) && isLayerVisible('wineries')){
+            map.addLayer(wineriesLayer);
+        }
+    }
 });
 
+var visibleLayers = ['viewpoints', 'sites', 'lodgings'];
+
+function isLayerVisible(layerGroupCode) {
+    return visibleLayers.indexOf(layerGroupCode) >= 0;
+}
+
+function addVisibleLayer(layerGroupCode) {
+    if(!isLayerVisible(layerGroupCode)){
+        visibleLayers.push(layerGroupCode);
+    }
+}
+
+function removeVisibleLayer(layerGroupCode) {
+    var index = visibleLayers.indexOf(layerGroupCode);
+    if(index >= 0) {
+        visibleLayers.splice(index, 1);
+    }
+}
+
 var switchableOptions = {
-    click: function(event, code, isChecked){
+    click: function(event, layerGroupCode, isChecked){
         var layerGroup = null;
-        if(code == 'viewpoints'){
+        if(layerGroupCode == 'viewpoints'){
             layerGroup = viewpointsLayer;
-        }else if(code == 'sites'){
+        }else if(layerGroupCode == 'sites'){
             layerGroup = sitesLayer;
-        }else if(code == 'lodgings'){
+        }else if(layerGroupCode == 'lodgings'){
             layerGroup = lodgingsLayer;
-        }else if(code == 'wineries'){
+        }else if(layerGroupCode == 'wineries'){
             layerGroup = wineriesLayer;
         }
         if(layerGroup){
+            if(isChecked){
+                addVisibleLayer(layerGroupCode);
+            }else{
+                removeVisibleLayer(layerGroupCode);
+            }
             if (map.hasLayer(layerGroup)) {
                 map.removeLayer(layerGroup);
-            }else{
+            }else if(isLayerVisible(layerGroupCode) && isChecked && map.getZoom() >= MAX_VISIBLE_ITEMS_ZOOM_LEVEL){
                 map.addLayer(layerGroup);
             }
         }
